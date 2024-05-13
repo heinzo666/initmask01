a5449b10271c3fd96d494928739997007c139a49
heinzo666 <166720679+heinzo666@users.noreply.github.com>
2024-05-13 20:28:17 +0700

    Update import.py

--- import.py
+++ import.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from git import Repo
 import datetime as dt
 tgl = dt.datetime.now().strftime("%y%m%d_%H%M%S")
@@ -7,6 +8,19 @@ os.chdir('../initmask')
 #shutil.make_archive('HEINZO', 'zip', '/content/images')
 full_local_path = "/content/initmask"
 
+initt = "/content/init.png"
+maskk = "/content/mask.png"
+initoutputss = "/content/initmask/outputs/init.png"
+maskoutputss = "/content/initmask/outputs/mask.png"
+shutil.copy(initt, initoutputss)
+shutil.copy(maskss, maskoutputss)
+
+inname = "/content/initmask/outputs/init.png"
+inn = ('/content/initmask/outputs/' + str(tgl) + 'I.png')
+os.rename(inname, inn)
+maname = "/content/initmask/outputs/mask.png"
+ma = ('/content/initmask/outputs/' + str(tgl) + 'M.png')
+os.rename(maname, ma)
 #backupname = "/content/training/HEINZO.zip"
 #backup = ('/content/training/H' + str(tgl) + '.zip')
 #os.rename(backupname, backup)
@@ -20,14 +34,5 @@ repo.index.commit("user_fakes")
 origin = repo.remote(name="origin")
 origin.push()
 
-#repo = Repo('/content')
-#repo.git.add('-A')
-#repo.git.commit('-a', '-m', 'commit')
-#remote = repo.remote(name='origin')
-#remote.push(refspec=(':delete_me'))
-
-#repo.git.remote.remove.origin
-
-#repo.git.push.origin.main
 
 os.chdir('..')
